6fd3863d049239908abeb3c56e7ffe401a2b4390
Quietontheiceberg <polypolypenguin@gmail.com>
2025-11-20 14:30:09 -0500

    Add files via upload

---
+++ Jessiemod4.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name:")
+grade = input("Please enter your grade:")
+grade = int(grade)
+if grade >= 59:
+    print("Hello", name, "you are passing")
+else:
+        print("Hello", name, "you are failing")
