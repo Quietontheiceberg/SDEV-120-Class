747b434b5e66e374ced61ac66ced95fe8611688d
Quietontheiceberg <polypolypenguin@gmail.com>
2025-12-13 13:58:03 -0500

    Add files via upload

---
+++ greater than less than.py
@@ -0,0 +1,12 @@
+def is_greater(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+a = 10
+b = 6
+
+result = is_greater(a, b)
+
+print("The statement", a, "is greater than", b, "is", result)
